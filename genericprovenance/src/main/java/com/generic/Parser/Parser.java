eeb63a04480430494244877e418a9b709b45fe90
Paulo Pintor <PPZimek@users.noreply.github.com>
2023-06-07 11:36:31 +0100

    07/06 - Multiple leves to Join done; - Group by done; - The Where-provenance still has problems because of the subselects and the alais;

--- genericprovenance/src/main/java/com/generic/Parser/Parser.java
+++ genericprovenance/src/main/java/com/generic/Parser/Parser.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.Alias;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.GroupByElement;
 import net.sf.jsqlparser.statement.select.Join;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
@@ -59,7 +63,8 @@ public class Parser {
         		projectionColumns = new ArrayList<ParserColumn>();
 				addProjectionColumns(plainSelect);
                 if (plainSelect.getJoins() != null && !plainSelect.getJoins().isEmpty()){
-                    JoinProvenance(plainSelect);
+                    plainSelect = JoinProvenance(plainSelect);
+                    selectStatement.setSelectBody(plainSelect);
 				}
 
             }
@@ -73,10 +78,10 @@ public class Parser {
         return result;
 	}
 
-	public PlainSelect JoinProvenance(PlainSelect plainSelect) throws JSQLParserException {
+	public PlainSelect JoinProvenance(PlainSelect plainSelect) throws JSQLParserException, AmbigousParserColumn {
         String provToken = "";
-
-        if(plainSelect.getFromItem() instanceof net.sf.jsqlparser.schema.Table)
+        
+        if(plainSelect.getFromItem() instanceof Table)
         {
             Table tempTable = (Table) plainSelect.getFromItem();
             this.JoinWhereColumns(plainSelect, true);
@@ -87,33 +92,51 @@ public class Parser {
             
             //TODO: falta verificar para UNIONS, DISTINCTS, etc
             if (tempPlainSelect.getJoins() != null && !tempPlainSelect.getJoins().isEmpty()){
-                JoinProvenance(tempPlainSelect);
+                tempSubSelect.setSelectBody(JoinProvenance(tempPlainSelect));
 			}else{
-                //tempSubSelect.setSelectBody(SelectProvenance(tempPlainSelect));
-                //Table tempTable = (Table) tempPlainSelect.getFromItem();
-                //provToken = (tempTable.getAlias() != null ? tempTable.getAlias() : tempTable.getName())+".prov";
+                this.SelectWhereColumns(tempPlainSelect, tempSubSelect.getAlias().getName());
+                tempSubSelect.setSelectBody(SelectProvenance(tempPlainSelect));
             }
-            //this.AddUnionColumns(plainSelect, true);
+            //[ ] confirmar: todos os subselects têm alias?
+            provToken = tempSubSelect.getAlias().getName()+".prov";
         }
 
         List<Join> joins = plainSelect.getJoins();
        
         for(Join join : joins) {
-            if(join.getRightItem() instanceof net.sf.jsqlparser.schema.Table){
+            
+            if(join.getRightItem() instanceof Table){
                 this.JoinWhereColumns(plainSelect, false);
             
                 Table tempTable = (Table) join.getRightItem();
                 provToken = provToken + "|| ' x ' ||"+(tempTable.getAlias() != null ? tempTable.getAlias() : tempTable.getName())+".prov";
             }
-            //else if(join.getRightItem() instanceof SubSelect)
-                //this.AddUnionColumns(plainSelect, false);
+            else if(join.getRightItem() instanceof SubSelect)
+            {
+                SubSelect tempSubSelect = (SubSelect) join.getRightItem();
+                PlainSelect tempPlainSelect = (PlainSelect) (tempSubSelect).getSelectBody();
+            
+                //TODO: falta verificar para UNIONS, DISTINCTS, etc
+                if (tempPlainSelect.getJoins() != null && !tempPlainSelect.getJoins().isEmpty()){
+                    tempSubSelect.setSelectBody(JoinProvenance(tempPlainSelect));
+                }else{
+                    this.SelectWhereColumns(tempPlainSelect, tempSubSelect.getAlias().getName());
+                    tempSubSelect.setSelectBody(SelectProvenance(tempPlainSelect));
+                }
+                
+                provToken = provToken + "|| ' x ' ||" + tempSubSelect.getAlias().getName()+".prov";
+            }
+            //this.AddUnionColumns(plainSelect, false);
         }
 
         SelectExpressionItem newColumn = new SelectExpressionItem();
         newColumn.setExpression(new net.sf.jsqlparser.schema.Column("'(' || "+provToken+" || ')' as prov"));
         plainSelect.addSelectItems(newColumn);
 
-        return plainSelect;
+        if(plainSelect.getGroupBy() != null){
+            return GroupByProvenance(plainSelect);
+        }else
+            return plainSelect;
     }
 
     /**
@@ -124,19 +147,69 @@ public class Parser {
      */
     private PlainSelect SelectProvenance(PlainSelect plainSelect) {
         SelectExpressionItem newColumn = new SelectExpressionItem();
-        newColumn.setExpression(new net.sf.jsqlparser.schema.Column("prov"));
+        Table tempTable = (Table) plainSelect.getFromItem();
+        String prov = (tempTable.getAlias() != null ? tempTable.getAlias() : tempTable.getName())+".prov";
+        newColumn.setExpression(new Column(prov));
         plainSelect.addSelectItems(newColumn);
 
         return plainSelect;
     }
 
     /**
-     * The function receives a PlainSelect and stores the table(s) and its column(s). The table and columns are traversed and compared with the projection column list to get the actual name of the tables and columns to use in the where provenance.
+     * The function is responsible to deal with Group Bys. It receives as parameter a PlainSelect and creates a new select with the Group By columns and the prov column, surronding the select on the PlainSelect.
+     * 
+     * @param plainSelect the select statement
+     * @return the select statement with the new Group By select and the prov column
+     */
+    private PlainSelect GroupByProvenance(PlainSelect groupBy){
+        PlainSelect newPlainSelect = new PlainSelect();
+        List<SelectItem> selectItems = new ArrayList<>();
+        ExpressionList gpList = groupBy.getGroupBy().getGroupByExpressionList();
+       
+        int index = 0;
+        Table tableTemp = null;
+        String _column = new String();
+
+        for(Expression ex : gpList.getExpressions()){
+            if(ex instanceof Column){
+                Column column = (Column) ex;
+                
+                if (index == 0){
+                    tableTemp = new Table("_"+column.getTable().getName());
+                    _column = tableTemp.getName()+"."+column.getColumnName();
+                }
+                column.setTable(tableTemp);
+                selectItems.add(new SelectExpressionItem(column));                
+            }
+            index++;
+        }
+        
+        String listAgg = getListAgg(tableTemp+".prov", '+', _column);
+        selectItems.add(new SelectExpressionItem(new Column(listAgg)));
+        newPlainSelect.addSelectItems(selectItems);
+
+        GroupByElement newGroupByElement = new GroupByElement();
+        newGroupByElement.setGroupByExpressionList(gpList);
+        newPlainSelect.setGroupByElement(newGroupByElement);
+
+       
+        SubSelect newSubSelect = new SubSelect();
+        groupBy.setGroupByElement(null);
+        newSubSelect.setSelectBody(groupBy);
+        Alias alias = new Alias(tableTemp.getName());
+        newSubSelect.setAlias(alias);
+        newPlainSelect.setFromItem(newSubSelect);
+        return newPlainSelect;
+    }
+
+    /**
+     * The function receives a PlainSelect that contains a JOIN and stores the table(s) and its column(s). Calls the function WhereColumns to compare with the projection columns and set the columns and tables' names.
      * 
      * @param plainSelect the select statement
      * @param isFromItem a boolean to check if it is the fromItem or the rightItem following the parser logic
+     * @throws AmbigousParserColumn
      */
-	private void JoinWhereColumns(PlainSelect plainSelect, boolean isFromItem){
+	private void JoinWhereColumns(PlainSelect plainSelect, boolean isFromItem) throws AmbigousParserColumn{
         Table tableTemp = new Table();
         
         //TODO: the JOINS can be more than one?
@@ -156,15 +229,48 @@ public class Parser {
 
         table.addColumns(plainSelect.getSelectItems());
 
+        WhereColumns(table, tableTemp.getAlias() != null ? tableTemp.getAlias().getName() : "");
+    }
+
+    /**
+     * The function receives a PlainSelect that contains a simple SELECT and stores the table(s) and its column(s). Calls the function WhereColumns to compare with the projection columns and set the columns and tables' names.
+     * 
+     * @param plainSelect the select statement
+     * @param alias the alias of from a subselect
+     * @throws AmbigousParserColumn
+     */
+    private void SelectWhereColumns(PlainSelect plainSelect, String alias) throws AmbigousParserColumn{
+        Table tableTemp = (Table) plainSelect.getFromItem();
+
+        ParserTable table = new ParserTable(tableTemp.getName(), tableTemp.getAlias() != null ? tableTemp.getAlias().getName() : null);
+
+        if(tableTemp.getSchemaName() != null) table.setSchema(tableTemp.getSchemaName());
+        if(tableTemp.getDatabase() != null) table.setDatabase(tableTemp.getDatabase().getDatabaseName());        
+
+        table.addColumns(plainSelect.getSelectItems());
+
+        alias = alias != null || !alias.isEmpty() ? alias : (tableTemp.getAlias() != null ? tableTemp.getAlias().getName() : "");
+
+        WhereColumns(table, alias);
+
+    }
+    
+    /**
+     * The function receives a ParserTable and a String alias, and compares the columns of the table with the projectionColumns list and sets the actual name of the columns and table to use in the where provenance.
+     * 
+     * @param table the table
+     * @param alias the alias of the table or the subselect
+     */
+    private void WhereColumns(ParserTable table, String alias){
         //if Alias is different of "", filter the projectionColumns
-        if(tableTemp.getAlias() != null)
+        if(!alias.isEmpty())
         {
-            String alias = tableTemp.getAlias().getName();
             List<ParserColumn> tempColumns = projectionColumns.stream().filter(column -> column.getTable().getName().compareTo(alias) == 0).collect(Collectors.toList());
 			
             for(ParserColumn c : tempColumns){
                 for(ParserColumn uc : table.getColumns()){
-                    if(c.getName() == uc.getAlias() || c.getName() == uc.getName())
+                    System.out.println(c.getName() + " " + uc.getAlias() + " " + uc.getName());
+                    if(c.getName().equals(uc.getAlias()) || c.getName().equals(uc.getName()))
                     {
                         c.setAlias(uc.getAlias());
                         c.setName(uc.getName());
@@ -175,7 +281,7 @@ public class Parser {
         }else{
             for(ParserColumn c : projectionColumns){
                 for(ParserColumn uc : table.getColumns()){
-                    if(c.getName() == uc.getAlias() || c.getName() == uc.getName())
+                    if(c.getName().compareTo(uc.getAlias()) == 0 || c.getName().compareTo(uc.getName()) == 0)
                     {
                         c.setAlias(uc.getAlias());
                         c.setName(uc.getName());
@@ -186,7 +292,7 @@ public class Parser {
         }
     }
 
-	/**
+    /**
 	 * The functions adds the main projection columns to the list of projection columns
 	 * 
 	 * @param select the PlainSelect representing the select statement
@@ -220,5 +326,17 @@ public class Parser {
 				index++;
             }            
         }
+    }
+
+    /**
+     * The function generates a String with the SQL Standard function 'ListAGG'. The expression is the column to be aggregated, the separator is the character to separate the values and the orderByColumn is the column to order the values.
+     * 
+     * @param expression the colum to be aggregated
+     * @param separator the character to separate the values
+     * @param orderByColumn the column to order the values
+     * @return a String with the SQL Standard function 'ListAGG'
+     */
+    private String getListAgg(String expression, char separator, String orderByColumn){
+        return String.format("listagg(%s, '%c') WITHIN GROUP (ORDER BY %s) AS prov", expression, separator, orderByColumn);
     }
 }
